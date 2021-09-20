7148e15df39759fe869babe93d44c385cf0c3668
Ayomide Omolewa <ayomideomolewa@gmail.com>
2021-09-20 18:35:58 -0500

    updated time

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 def daily_job(update, context):
     """ Running on Mon, Tue, Wed, Thu, Fri = tuple(range(5)) """
     context.bot.send_message(chat_id=CHAT_ID, text='Activating daily paramount notification!')
-    sport = datetime.time(18, 35, 10, 000000, tzinfo=pytz.timezone('America/Chicago'))
+    sport = datetime.time(18, 38, 10, 000000, tzinfo=pytz.timezone('America/Chicago'))
     trading = datetime.time(15, 00, 10, 000000, tzinfo=pytz.timezone('America/Chicago'))
     forex = datetime.time(21, 00, 10, 000000, tzinfo=pytz.timezone('America/Chicago'))
     print("Time its supposed to post", sport)
