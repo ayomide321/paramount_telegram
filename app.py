73262441af3da4e5c80b3e05c5b9566ef515e30f
Ayomide Omolewa <ayomideomolewa@gmail.com>
2021-09-20 17:10:28 -0500

    added requirements

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from telegram.ext import Updater, CommandHandler
-from telegram import bot
+from telegram import bot, ParseMode
 import os
 import datetime
+import pytz
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -12,12 +13,14 @@ CHAT_ID = os.getenv('CHAT_ID')
 
 def daily_job(update, context):
     """ Running on Mon, Tue, Wed, Thu, Fri = tuple(range(5)) """
-    context.bot.send_message(chat_id=CHAT_ID, text='Setting daily paramount notification!')
-    t = datetime.time(3, 16, 00, 000000)
+    context.bot.send_message(chat_id=CHAT_ID, text='Activating daily paramount notification!')
+    t = datetime.time(17, 2, 10, 000000, tzinfo=pytz.timezone('America/Chicago'))
+    print("Time its supposed to post", t)
+    print("Time right now:", datetime.datetime.now())
     context.job_queue.run_daily(purchase, t, days=tuple(range(5)), context=update)
 
 def purchase(context):
-    context.bot.send_message(chat_id=CHAT_ID, text="Emjoy our services? Click <a href="">here</a> to purchase a Paramoutn forex subscription!", parse_mode=ParseMode.HTML)
+    context.bot.send_message(chat_id=CHAT_ID, text="Enjoy our services? Click <a href='https://pstrading.online/forex'>here</a> to purchase a Paramount forex subscription!", parse_mode=ParseMode.HTML)
 
 u = Updater(TOKEN, use_context=True)
 u.dispatcher.add_handler(CommandHandler('purchase', daily_job))
